838b130ec58d56c71d4ec5e9159fd41142e2ddab
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-02-25 19:48:13 +0900

    Initial version

---
+++ README.md
@@ -0,0 +1,16 @@
+# C/C++ Completion for deoplete using clang
+
+#### NOTE: Python (>= 3.4) is needed
+
+## Install
+
+* Install the latest deoplete.nvim
+* Make sure you already have clang(`clang` command)
+
+## Todo
+
+* .clang, .clang_complete file support
+
+* compile_commands.json file support
+
+* options support
---
+++ rplugin/python3/deoplete/source/clangx.py
@@ -0,0 +1,98 @@
+# =============================================================================
+# FILE: clangx.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# =============================================================================
+
+import re
+import os.path
+import subprocess
+from itertools import chain
+
+from deoplete.util import getlines
+# from deoplete.util import error
+from .base import Base
+
+
+class Source(Base):
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'clangx'
+        self.filetypes = ['c', 'cpp']
+        self.mark = '[clangx]'
+        self.rank = 500
+        self.executable_clang = self.vim.call('executable', 'clang')
+        self.encoding = self.vim.eval('&encoding')
+        self.input_pattern = r'\.[a-zA-Z0-9_?!]*|[a-zA-Z]\w*::\w*|->\w*'
+
+    def get_complete_position(self, context):
+        m = re.search('[a-zA-Z0-9_]*$', context['input'])
+        return m.start() if m else -1
+
+    def gather_candidates(self, context):
+        if not self.executable_clang:
+            return []
+
+        line = context['position'][1]
+        column = context['complete_position']
+        lang = 'c++' if context['filetype'] == 'cpp' else 'c'
+        args = [
+            'clang', '-x', lang, '-fsyntax-only',
+            '-Xclang', '-code-completion-macros',
+            '-Xclang', '-code-completion-at=-:{}:{}'.format(line, column),
+            '-',
+            '-I', os.path.dirname(context['bufpath']),
+        ]
+        args += self._args_from_neoinclude(context)
+        buf = '\n'.join(getlines(self.vim)).encode(self.encoding)
+        try:
+            proc = subprocess.Popen(args=args,
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.DEVNULL,
+                                    cwd=context['cwd'])
+            result, errs = proc.communicate(buf, timeout=10)
+            result = result.decode(self.encoding)
+        except subprocess.TimeoutExpired as e:
+            proc.kill()
+            return []
+        return self._parse_lines(result.splitlines())
+
+    def _args_from_neoinclude(self, context):
+        if not self.vim.call(
+                'exists', '*neoinclude#get_path'):
+            return []
+
+        return list(chain.from_iterable(
+            [['-I', x] for x in
+             self.vim.call('neoinclude#get_path',
+                           context['bufnr'],
+                           context['filetype']).replace(';', ',').split(',')
+             if x != '']))
+
+    def _parse_lines(self, lines):
+        candidates = []
+        for line in lines:
+            m = re.search('^COMPLETION:\s+(.{,}?) : (.{,}?)$', line)
+            if not m:
+                m = re.search('^COMPLETION:\s+(.*)$', line)
+                if m:
+                    candidates.append({'word': m.group(1)})
+                continue
+
+            menu = m.group(2)
+            menu = menu.replace('[#', '')
+            menu = menu.replace('#]', '')
+            menu = menu.replace('<#', '')
+            menu = menu.replace('#>', '')
+            menu = menu.replace('{#', '')
+            menu = menu.replace('#}', '')
+
+            word = m.group(1)
+            if word.startswith('PFNG'):
+                continue
+
+            candidate = {'word': word, 'dup': 1}
+            if menu != word:
+                candidate['menu'] = menu
+        return candidates
